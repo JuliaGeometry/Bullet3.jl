9afaf66ba23824a81dbc69025e58db039e3429c3
SimonDanisch <sdanisch@gmail.com>
2018-07-26 12:30:39 +0200

    make sure build works

--- src/Bullet3.jl
+++ src/Bullet3.jl
@@ -1,5 +1,13 @@
 module Bullet3
 
 # package code goes here
+const file = joinpath(@__DIR__, "..", "deps", "deps.jl")
+isfile(file) || error("Bullet3 not build correctly. Please run Pkg.build(\"Bullet3\")")
+
+include(file)
+
+function __init__()
+    check_deps()
+end
 
 end # module
